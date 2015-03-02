4b476bbd919fc1dc759b2cf5c42bf75d64c4bce0
moslisnas <molicapacidad@gmail.com>
2015-03-02 15:53:44 +0100

    Update and rename js to js/buttons.js

---
+++ js/buttons.js
@@ -0,0 +1,11 @@
+function cambiarLocationLogin1(){
+	window.location="inicio.html";
+}
+
+function cambiarLocationLogin2(){
+	window.location="crearcuenta.html";
+}
+
+function cambiarLocationRegistro(){
+	window.location="login.html";
+}
